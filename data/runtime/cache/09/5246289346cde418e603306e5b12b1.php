fcd3771c1ebb6fd3507ca3aa78414e0367814d64
1 <test111@qq.com>
2026-02-08 22:45:25 +0800

    删除

--- data/runtime/cache/09/5246289346cde418e603306e5b12b1.php
+++
@@ -1,4 +0,0 @@
-<?php
-//000000000000
- exit();?>
-think_serialize:a:5:{s:9:"accessKey";s:20:"HPUAOJGAONJFLUQFMNTK";s:9:"secretKey";s:40:"SCKahJI2sZRGLPAhmQcziq4n7psWB7JzPOpN00oC";s:8:"endPoint";s:43:"http://obs.ap-southeast-1.myhuaweicloud.com";s:6:"Bucket";s:13:"live-peter-ii";s:6:"domain";s:58:"https://live-peter-ii.obs.ap-southeast-1.myhuaweicloud.com";}
--- data/runtime/cache/a6/045c7fa446c63734fd2f83f333acf7.php
+++
@@ -1,4 +0,0 @@
-<?php
-//000000000000
- exit();?>
-think_serialize:a:3:{s:9:"max_files";s:2:"20";s:10:"chunk_size";s:3:"512";s:10:"file_types";a:4:{s:5:"image";a:2:{s:19:"upload_max_filesize";s:5:"10240";s:10:"extensions";s:26:"jpg,jpeg,png,gif,bmp4,svga";}s:5:"video";a:2:{s:19:"upload_max_filesize";s:6:"102400";s:10:"extensions";s:3:"mp4";}s:5:"audio";a:2:{s:19:"upload_max_filesize";s:5:"10240";s:10:"extensions";s:3:"mp3";}s:4:"file";a:2:{s:19:"upload_max_filesize";s:6:"102400";s:10:"extensions";s:43:"txt,pdf,doc,docx,xls,xlsx,ppt,pptx,svga,mp4";}}}
