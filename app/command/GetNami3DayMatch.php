c6313d03d74dc1dd776fbc9666f893d84fd5c0fc
Alice <alice@example.com>
2026-04-03 22:14:20 +0800

    绑定主播

--- app/command/GetNami3DayMatch.php
+++ app/command/GetNami3DayMatch.php
@@ -130,7 +130,7 @@ class GetNami3DayMatch extends Command
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);    // 要求结果为字符串且输出到屏幕上
         curl_setopt($ch, CURLOPT_HEADER, 0); // 不要http header 加快效率
         curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (compatible; MSIE 5.01; Windows NT 5.0)');
-        curl_setopt($ch, CURLOPT_TIMEOUT, 15);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 60);
         $output = curl_exec($ch);
         curl_close($ch);
         return $output;
